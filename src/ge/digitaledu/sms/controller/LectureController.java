6b1384c056f5d4984ffa19fa16463b94d68804fd
Misha <mikheil.nanobashvili@gmail.com>
2019-08-01 20:44:11 +0400

    test

--- src/ge/digitaledu/sms/controller/LectureController.java
+++ src/ge/digitaledu/sms/controller/LectureController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//git status
+//git test
 
 public class LectureController extends MainController {
 
