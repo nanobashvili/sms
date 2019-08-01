d79e2262af8d5299990b7fb5b79c76e6c735db84
Misha <mikheil.nanobashvili@gmail.com>
2019-08-01 20:38:17 +0400

    git

--- src/ge/digitaledu/sms/controller/LectureController.java
+++ src/ge/digitaledu/sms/controller/LectureController.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//git status
+
 public class LectureController extends MainController {
 
     @Override
